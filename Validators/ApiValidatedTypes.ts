584934a9a6c553956eb0baba2817de34aa88f46d
Mehdi Souayed <99084221+LordSizzz@users.noreply.github.com>
2023-02-18 21:19:05 +0000

    Started Validators and Database

--- Validators/ApiValidatedTypes.ts
+++ Validators/ApiValidatedTypes.ts
@@ -1,21 +1,90 @@
 import { z } from "zod"
 
-export const ProductAddReqSchema=z.object({
+export const UserCreateReqSchema=z.object({
+    body:z.object({
+    id:z.string(),
+    nom:z.string(),
+    email:z.string().email(),
+    password:z.string(),
+    birthday:z.string().dateTime()
+  }
+})
+export type UserCreateReq=z.infer<typeof userCreateReq>
+
+export const UserSchema=z.object({
+  id:z.string(),
+  nom:z.string(),
+  email:z.string().email(),
+  password:z.string(),
+  birthday:z.string().dateTime(),
+  podcasts:z.string().array(),
+  comments:z.string().array(),
+});
+export type User=z.infer<typeof UserSchema>
+
+export const CommentSchema=z.object({
+  id:z.string(),
+  content:z.string(),
+  valid:z.boolean(),
+  password:z.string(),
+  birthday:z.string().dateTime(),
+  podcasts:z.string().array(),
+  comments:z.string().array(),
+});
+export type Comment=z.infer<typeof CommentSchema>
+
+export const CommentSchema=z.object({
+  id:z.string(),
+  content:z.string(),
+  valid:z.boolean(),
+  password:z.string(),
+  birthday:z.string().dateTime(),
+  podcasts:z.string().array(),
+  comments:z.string().array(),
+});
+export type Comment=z.infer<typeof CommentSchema>
+
+export const CommentReqSchema=z.object({
   body:z.object({
     id:z.string(),
-    name:z.string(),
-    price:z.string().or(z.number().positive()),
-    description:z.string().optional()
+    content:z.string(),
+    valid:z.boolean(),
+    password:z.string(),
+    birthday:z.string().dateTime(),
+    podcasts:z.string().array(),
+    comments:z.string().array(),
   }),
   headers:z.object({
-  authorization:z.string()
-  })
-})
-export type ProductAddReq=z.infer<typeof ProductAddReqSchema>
-
-export const AccessReqSchema=z.object({
-  headers:z.object({
-  authorization:z.string()
+    authorization:z.string()
   })
 });
-export type AccessReq=z.infer<typeof AccessReqSchema>
+export type CommentReq=z.infer<typeof CommentReqSchema>
+
+export const PodcastSchema=z.object({
+  id:z.string(),
+  content:z.string(),
+  valid:z.boolean(),
+  password:z.string(),
+  birthday:z.string().dateTime(),
+  podcasts:z.string().array(),
+  comments:z.string().array(),
+});
+export type Podcast=z.infer<typeof PodcastSchema>
+
+export const PodcastReqSchema=z.object({
+  body:z.object({
+    id:z.string(),
+    content:z.string(),
+    valid:z.boolean(),
+    password:z.string(),
+    birthday:z.string().dateTime(),
+    podcasts:z.string().array(),
+    comments:z.string().array(),
+  }),
+  headers:z.object({
+    authorization:z.string()
+  })
+});
+export type PodcastReq=z.infer<typeof PodcastReqSchema>
+
+
